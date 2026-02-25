6e4e4939f9b787879e923534881de7bc4ad68bc7
Oksana <oksanamikhalyncuk@gmail.com>
2026-02-25 17:06:58 +0200

    Initial commit

---
+++ lab1/script.js
@@ -0,0 +1,11 @@
+// знаходимо елемент h1
+let heading = document.getElementById("title");
+
+// виводимо Hello world!
+heading.textContent = "Hello world!";
+
+
+// функція для кнопки
+function showStudentName() {
+    console.log("Оксана"); // впиши своє ім'я
+}
